ea88871844c8ca0b32cb0d849636ea98758f4cb4
Carlysle Price <carlysle@codebycarl.com>
2015-01-29 03:57:18 -0500

    added bootstrap + minor php for header and footer

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+	<footer>
+		<p id="footer-tagline"><?php echo CODENAME . " | " . TAGWORD . " in " . LOCATION;?></p>
+		<br>
+		<p>&copy; <?php echo date("Y") . " " . DEVELOPER;?> | Web Developer</p>
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once("/inc/config/config.php");
+$pageTitle = DEVELOPER . " | " . "Web Developer";
+$section = "index";
+include("/inc/header.php");
+?>
+
+	<div class="container">
+		<h1>Hello World!</h1>
+	</div>
+<?php include("/inc/footer.php");?>
