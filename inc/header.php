b0757877555748667ac859af612dda2de43a981e
Carlysle Price <carlysle@codebycarl.com>
2015-01-29 04:19:01 -0500

    added basic navbar

--- inc/header.php
+++ inc/header.php
@@ -11,6 +11,23 @@
 </head>
 <body>
 	<header>
-		<nav>
+		<nav class="navbar navbar-default navbar-static-top">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<i class="sr-only">Toggle navigation</i>
+					<i class="icon-bar"></i>
+					<i class="icon-bar"></i>
+					<i class="icon-bar"></i>
+				</button>
+				<p><a class="navbar-brand" href="#"><i class="glyphicon glyphicon-qrcode" id="qr"></i><?php echo CODENAME;?></a><span id="dotcom">.com</span></p>
+			</div>
+			<div class="collapse navbar-collapse" id="navbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"><i class="glyphicon glyphicon-user nvbr-glyph"></i>About</a></li>
+        			<li><a href="#"><i class="glyphicon glyphicon-folder-open nvbr-glyph"></i>Portfolio</a></li>
+        			<li><a href="#"><i class="glyphicon glyphicon-stats nvbr-glyph"></i>Skills</a></li>
+        			<li><a href="#"><i class="glyphicon glyphicon-comment nvbr-glyph"></i>Contact</a></li>
+				</ul>
+			</div>
 		</nav>
 	</header>
