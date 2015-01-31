b3585f7e71e1ede5feaaaa354225d1548949453e
Carlysle Price <carlysle@codebycarl.com>
2015-01-31 02:13:45 -0500

    minor contact form php + sanitize function

--- contact/index.php
+++ contact/index.php
@@ -3,35 +3,48 @@ require_once("../inc/config/config.php");
 $pageTitle = DEVELOPER . " | " . "Contact Me";
 $section = "contact";
 include("../inc/header.php");
-?>
+// form stuff
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["inputName"])) {
+    	$nameErr = "Name is required";
+ 	} else {
+    	$name = xxx($_POST["inputName"]);
+  	}
+  $name = xxx($_POST["inputName"]);
+  $email = xxx($_POST["inputEmail"]);
+  $reason = xxx($_POST["inputInquiry"]);
+  $message = xxx($_POST["inputMessage"]);
+}?>
 
 	<div class="container">
-		<h1>Contact Me!</h1>
-		<form role="form">
+		<h1 class="text-center">Contact Me!</h1>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" role="form">
 			<div class="wrapper">
 				<div class="form-group has-feedback has-feedback-left">
 					<label for="inputName" class="control-label">Name</label>
-					<input type="text" placeholder="Enter Your Name" class="form-control" id="inputName">
+					<input type="text" placeholder="Enter Your Name" value="<?php if (isset($name)) echo $name;?>" class="form-control" name="inputName">
 					<span class="glyphicon glyphicon-user form-control-feedback"></span>
 				</div>
 				<div class="form-group has-feedback has-feedback-left">
 					<label for="inputEmail" class="control-label">Email</label>
-					<input type="text" placeholder="Enter You Email Address" class="form-control" id="inputEmail">
+					<input type="text" placeholder="Enter You Email Address" value="<?php if (isset($email)) echo $email;?>" class="form-control" name="inputEmail">
 					<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
 				</div>
 				<div class="form-group">
-					<label for="inputInqury">Reason for inquiry</label>
-					<select name="inputInqury" class="form-control" id="inputInquiry">
+					<label for="inputInquiry">Reason for inquiry</label>
+					<select class="form-control" name="inputInquiry">
 						<option value="General">General</option>
-						<option value="Freelance">Freelance/Employment (Paid Work)</option>
+						<option value="Freelance">Freelance / Employment (Paid Work)</option>
 						<option value="Collab">Collaboration (Unpaid Work)</option>
 					</select>
 				</div>
 				<div class="form group">
 					<label for="inputMessage" class="control-label">Message</label>
-					<textarea placeholder="Write Your Message" class="form-control" rows="3" id="inputMessage"></textarea>
+					<textarea placeholder="Write Your Message" value="<?php if (isset($message)) echo $message;?>"class="form-control" rows="3" name="inputMessage"></textarea>
 				</div>
-				<input type="submit" class="btn btn-primary" value="Submit Button">
+				<div class="form-group" id="submit-btn">
+					<input type="submit" class="btn btn-primary btn-lg center-block" value="Submit Button">
+				</div>
 			</div>
 		</form>
 	</div>
